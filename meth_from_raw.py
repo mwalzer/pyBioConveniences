f5659565c7228bb74ffaaf3c01e40df4ba2f80da
Mathias Walzer <walzer@informatik.uni-tuebingen.de>
2014-12-08 19:24:12 +0100

    Update meth_from_raw.py

--- meth_from_raw.py
+++ meth_from_raw.py
@@ -1,23 +1,52 @@
+__author__ = 'walzer'
+
+#! /usr/bin/python
+
+import sys
+import argparse
 import re
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile, isdirectory, join
+import pickle
 
-mypath = '.'
-onlyfiles = [ f for f in listdir(mypath) if ( isfile(join(mypath,f)) and (f.endswith('.RAW') or f.endswith('.raw') ) and (not f.startswith('~')))]
-
-results = dict()
-for of in onlyfiles:
-  f = open(of, "rb")
-  met_regex = re.findall( b'\x00\x43\x00\x3A\x00(.*?)\x00\x6D\x00\x65\x00\x74\x00\x68\x00\x00\x00\x00', f.read() )
-  f.close()
-  #f1
-  #['\\\x00X\x00c\x00a\x00l\x00i\x00b\x00u\x00r\x00\\\x00m\x00e\x00t\x00h\x00o\x00d\x00s\x00\\\x00D\x00i\x00o\x00n\x00e\x00x\x00 \x00R\x00S\x00L\x00C\x00n\x00a\x00n\x00o\x00\\\x00W\x00i\x00t\x00h\x00o\x00u\x00t\x00 \x00U\x00V\x00 \x00D\x00e\x00t\x00e\x00c\x00t\x00i\x00o\x00n\x00\\\x00t\x00o\x00p\x005\x00C\x00I\x00D\x00_\x004\x000\x000\x00-\x006\x005\x000\x00m\x00z\x00_\x005\x000\x00c\x00m\x00R\x00S\x00L\x00C\x00_\x00s\x00t\x00e\x00e\x00p\x00e\x00r\x00.']
-  try:
-    method_name = met_regex[0].replace('\x00','').split('\\')[-1] + 'meth'
-    results[of] = method_name
-  except:
-    results[of] = 'n/a'
+def access_meth(rf):
+	if isfile(rf):
+		met_regex = None
+		with open(rf, 'rb') as f:
+			met_regex = re.findall( b'\x00\x43\x00\x3A\x00(.*?)\x00\x6D\x00\x65\x00\x74\x00\x68\x00\x00\x00\x00', f.read() )
+		try:
+			method_name = met_regex[0].replace('\x00','').split('\\')[-1] + 'meth'
+			return method_name
+		except:
+			pass
+	return None
 
 
-
+def __main__():
+	parser = argparse.ArgumentParser(description='Read method file name from raw file - not recursive')
+	parser.add_argument('-in','--input', help='Raw file or directory with files to read raw from.', required=True)
+	parser.add_argument('-p','--pickle', help='Pickle for result dictionary, no console output.', required=False)
+	if len(sys.argv)==1:
+		parser.print_help()
+		sys.exit(1)
+	args = vars(parser.parse_args())
+	if not args['in']:
+		parser.print_help()
+		
+	if isfile(args['in']) and (args['in'].endswith('.RAW') or args['in'].endswith('.raw') ):
+		m = access_meth(args['in'])
+		if args['p']:
+			pickle.dump( {args['in']:m}, open( args['p'], "wb" ) )
+		else:
+			print args['in'], ":", m
+	else if isdirectory(args['in']):
+		onlyrawfiles = [ f for f in listdir(args['in']) if ( isfile(join(args['in'],f)) and (f.endswith('.RAW') or f.endswith('.raw') ) and (not f.startswith('~')))]
+		m = {f: access_meth(f) for f in onlyrawfiles}
+		if args['p']:
+			pickle.dump( m, open( args['p'], "wb" ) )
+		else:
+			for p in m:
+				print p, ":", m[p]
+	else:
+		parser.print_help()
 
