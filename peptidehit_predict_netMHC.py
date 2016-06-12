d74c7fcfdd663c0c833d1de351e37cc2a1182309
mwalzer <walzer@informatik.uni-tuebingen.de>
2016-06-12 15:32:19 +0200

    added allele read in to peptidehit_predict_.. , bumped minor version

--- peptidehit_predict_netMHC.py
+++ peptidehit_predict_netMHC.py
@@ -5,14 +5,14 @@ import os
 from datetime import datetime
 import logging
 import argparse
-import pickle
 import pyopenms as oms
 from os.path import join
 from Fred2.Core.Allele import Allele
 from Fred2.Core.Peptide import Peptide
 from Fred2.EpitopePrediction import EpitopePredictorFactory
+from Fred2.IO import FileReader
 
-VERSION = "0.2"
+VERSION = "0.3"
 
 def categorize(score):
     if score >= 0.6384377847127609:
@@ -24,8 +24,9 @@ def categorize(score):
 
 def __main__():
     parser = argparse.ArgumentParser(version=VERSION)
-    parser.add_argument('-in',  dest="inf", help='<Required> full path to the input idXML', required=True)
-    parser.add_argument('-out', dest="out", help="<Required> full path to the output idXML", required=True)
+    parser.add_argument('-in',  dest="inf", help='<Required> full path to the input file', required=True)
+    parser.add_argument('-out', dest="out", help="<Required> full path to the output file", required=True)
+    parser.add_argument('-allele', dest="allele", help="<Required> full path to an allele file", required=True)
 
     options = parser.parse_args()
     if len(sys.argv) <= 1:
@@ -35,50 +36,32 @@ def __main__():
     if not (options.inf or options.out):
         parser.print_help()
         sys.exit(1)
-    #~ else:
-    #~ print dir(options)
 
-    #~ if options.temp_path:
-        #~ temp_dir = options.temp_path
-    #~ else:
-        #~ temp_dir = "/tmp/"
+    target_alleles_set = set()
+    if not (options.allele):
+        #Fred2.FileReader.read_lines is broken
+        #alleles = FileReader.read_lines(options.allele, type=Allele)
+        with open(options.allele, 'r') as handle:
+            for line in handle:
+                target_alleles_set.add(Allele(line.strip().upper()))
 
-    if "Tue39L243" in options.inf:
-        print "class2 run recognized", options.inf
+    if not target_alleles_set:
+        parser.print_help()
         sys.exit(1)
-
-    donordir = {"BD-ZH03": ['A*01:01','A*11:01','B*15:01','B*35:01','C*03:03','C*04:01'], 
-    "BD-ZH02": ['A*11:01','A*68:01','B*15:01','B*35:03','C*03:03','C*04:01'],
-    "BD-ZH01": ['A*02:01','A*11:01','B*27:05','B*51:01','C*01:02','C*15:02'], 
-    "BD-ZH06": ['A*03:01','A*68:02','B*07:02','B*14:02','C*07:02','C*08:02']}
-
+        
     ttn = EpitopePredictorFactory('netmhc')
-    target_alleles_net = None
-    for do in donordir:
-        if do in options.inf:
-            target_alleles_net = donordir[do]
-
-    if not target_alleles_net:
-        print "no donor recognized", options.inf
-        sys.exit(1)
 
     pros = list()
     peps = list()
     f = oms.IdXMLFile()
-    #~ f.load(join(path, idf), pros, peps)
     f.load(options.inf, pros, peps)
 
     pepstr = set()
     for pep in peps:
         for h in pep.getHits():
             if "decoy" not in h.getMetaValue("target_decoy"):
-                ps = h.getSequence().toUnmodifiedString()
-                if 7 < len(ps) < 12:
-                    if "U" not in ps:
-                        pepstr.add(h.getSequence().toUnmodifiedString())
-
-    hla_pref = 'HLA-'
-    target_alleles_net_a = [Allele(hla_pref + x) for x in target_alleles_net]
+                if 7 < len(h.getSequence().toUnmodifiedString()) < 12:
+                    pepstr.add(h.getSequence().toUnmodifiedString())
 
     es = [Peptide(x) for x in pepstr]
 
@@ -86,7 +69,6 @@ def __main__():
         preds_n = ttn.predict(es, alleles=target_alleles_net_a)
     except Exception as e:
         print "something went wrong with the netMHC prediction", options.inf, "what:",  str(e)
-        pickle.dump(pepstr, open( '.'.join(options.out.split(".")[:-1]) + ".pkl", "wb" ) )
         sys.exit(1)
 
     preds = dict()    
@@ -112,7 +94,6 @@ def __main__():
                 nhits.append(h)
         pep.setHits(nhits)                    
 
-    #~ f.store(join(path, odf), pros, peps)
     f.store(options.out, pros, peps)
 
 
